1f3752ca870089a6bbab276fff0488ce9c6744a5
Saranya Balasubramanian <Saranya.Balasubramanian@oeaw.ac.at>
2019-08-01 17:28:49 +0200

    removed vocabs app and

--- tieromina/tieromina/urls.py
+++ tieromina/tieromina/urls.py
@@ -2,18 +2,13 @@ from django.conf.urls import url, include, handler404
 from django.contrib import admin
 from django.conf import settings
 from rest_framework import routers
-from vocabs import api_views
+
 
 if 'bib' in settings.INSTALLED_APPS:
     from bib.api_views import ZotItemViewSet
 
 router = routers.DefaultRouter()
-router.register(r'metadata', api_views.MetadataViewSet)
-router.register(r'skoslabels', api_views.SkosLabelViewSet)
-router.register(r'skosnamespaces', api_views.SkosNamespaceViewSet)
-router.register(r'skosconceptschemes', api_views.SkosConceptSchemeViewSet)
-router.register(r'skoscollections', api_views.SkosCollectionViewSet)
-router.register(r'skosconcepts', api_views.SkosConceptViewSet)
+
 if 'bib' in settings.INSTALLED_APPS:
     router.register(r'zotitems', ZotItemViewSet)
 
@@ -23,8 +18,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^browsing/', include('browsing.urls', namespace='browsing')),
     url(r'^info/', include('infos.urls', namespace='info')),
-    url(r'^vocabs/', include('vocabs.urls', namespace='vocabs')),
-    url(r'^vocabs-ac/', include('vocabs.dal_urls', namespace='vocabs-ac')),
     url(r'^', include('webpage.urls', namespace='webpage')),
 ]
 
