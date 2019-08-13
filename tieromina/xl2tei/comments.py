d9103f7a34aec362398e4384101bfe6ca13da4a5
gythaogg <gythaoggscat@gmail.com>
2019-08-13 12:04:40 +0200

    comments correctly added

--- tieromina/xl2tei/comments.py
+++ tieromina/xl2tei/comments.py
@@ -2,10 +2,11 @@ class Comments:
     '''
     Philological commentary on the omen
     '''
-    label = 'Philological Commentary'
-    reference = ''
-    text = []
-    
+    def __init__(self):
+        self.label = 'Philological Commentary'
+        self.reference = ''
+        self.text = []
+        
     def append(self, row):
         '''
         Appends to the text of the comment
@@ -14,7 +15,7 @@ class Comments:
         for cell in row[1:]:
             if cell.value: row_text = row_text + ' ' + cell.value if row_text else cell.value
 
-        self.text = row_text
+        self.text.append(row_text)
         return
                     
                 
--- tieromina/xl2tei/tests/test_comments.py
+++ tieromina/xl2tei/tests/test_comments.py
@@ -4,17 +4,29 @@ import xlrd
 from ..comments import Comments
 
 
-class TabletTestCase(TestCase):
-    def setUp(self):        
+class CommentsTestCase(TestCase):
+    def setUp(self):
         wb = xlrd.open_workbook('xl2tei/tests/Snake_23_1_11.xls',formatting_info=True)
         self.sheet = wb.sheet_by_index(0)
-        self.comment = Comments()
         return
-
-
+  
     def test_empty_comment(self):
-        self.assertEqual(self.comment.label, 'Philological Commentary')
-        self.assertEqual(self.comment.reference, '')
-        self.assertEqual(self.comment.text, [])
+        comments = Comments()
+        self.assertEqual(comments.label, 'Philological Commentary')
+        self.assertEqual(comments.reference, '')
+        self.assertEqual(comments.text, [])
         return
-        
+    
+    def test_one_line(self):
+        comments = Comments()
+        comments.append(self.sheet.row(21))
+        self.assertEqual(comments.text, ['According to KAL 1, p. 3, SU 51/49+ cannot be considered “canonical, but are merely very close to the canonical series (cf. ibd, p. 6, fn. 57).'])
+        self.assertEqual(len(comments.text), 1)
+        return
+
+    def test_multiple_lines(self):
+        comments = Comments()
+        comments.append(self.sheet.row(21))
+        comments.append(self.sheet.row(22))
+        self.assertEqual(comments.text[0], 'According to KAL 1, p. 3, SU 51/49+ cannot be considered “canonical, but are merely very close to the canonical series (cf. ibd, p. 6, fn. 57).')
+        return
