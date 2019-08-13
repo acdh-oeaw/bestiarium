47375b0a4de02adbd881f0b372d2adae16b3dff4
gythaogg <gythaoggscat@gmail.com>
2019-08-13 23:58:19 +0200

    not working yet

--- tieromina/xl2tei/sheet.py
+++ tieromina/xl2tei/sheet.py
@@ -1,5 +1,6 @@
 import logging
 from .comments import Comments
+from .reading import Readings
 from .score import Score
 
 class Sheet:
@@ -8,21 +9,50 @@ class Sheet:
     contains score, readings and commentary
     '''
     chapter = ''
-    readings = []
-    comments = Comments()
-    unknown = []
-    witnesses = []
+
     
     def __init__(self, sheet, line_num_format=None):
         self.sheet = sheet
         self.line_num_format = line_num_format
-        self.score = Score(self.line_num_format)
-        self.classify_rows()
-        if self.unknown:
-            logging.warning('Found the following rows with no label - SKIPPED\n%s', self.unknown)
 
         return
-                    
+
+    def read(self):
+        '''
+        Reads the score first, 
+        identifies 
+        columns containing tokens and 
+        columns containing line numbers
+        Reads readings
+        Then reads commentary
+        '''
+        
+        self.comments = Comments()
+        self.unknown = []
+
+        def read_until(start_row_num=0, end_label_pattern=None):
+            for row_num in range(start_row_num, self.sheet.nrows):
+                row = self.sheet.row(row_num)
+                row_label = row[0].value
+                if self._is_empty(row): continue
+                elif end_label_pattern and  end_label_pattern in row_label.lower():                    
+                    yield row_num, None
+                else: yield row_num, row
+
+            return
+                
+        self.score = Score(self.line_num_format)
+        for row_num, row in self.read_until(start_row_num=1, end_label_pattern='(trl)'):
+            if row: self.score.append(row)
+
+        self.readings = Readings()
+        for row_num, row in self.read_until(start_row_num=row_num, end_label_pattern='comment'):
+            self.readings.append(row)
+            
+        
+            
+            
+        
     def classify_rows(self):
         '''
         Classifies rows into score/readings/comment or unknown.
@@ -31,9 +61,8 @@ class Sheet:
         unknown in BAD! :-x
         '''
         comment_started = False
-        for row_num in range(1, self.sheet.nrows):
-            row = self.sheet.row(row_num)
-            row_label = row[0].value
+        
+
             if self._is_empty(row):
                 continue
             
@@ -43,17 +72,16 @@ class Sheet:
             elif comment_started:
                 self.comments.append(row)
 
-            elif 'comment' in row_label.lower():
-                comment_started = True
-                self.comments.append(row)
+            el
                 
             elif '(' in row_label and ')' in row_label:
                 self.readings.append(row)
 
             else:
                 self.score.append(row)
-                
-          
+        
+        return
+    
     @staticmethod
     def _is_empty(row):
         '''
--- tieromina/xl2tei/tests/test_sheet.py
+++ tieromina/xl2tei/tests/test_sheet.py
@@ -16,8 +16,10 @@ class SheetTestCase(TestCase):
         return
 
     def test_comment_lines(self):
-        expected_comment = Comments()
-        expected_comment.append(self.wb_sheet.row(21))
-        expected_comment.append(self.wb_sheet.row(22))
-        self.assertEqual(self.sheet .comments.__dict__, expected_comment.__dict__)
+        self.assertEqual(self.sheet.comments.text[0], 'According to KAL 1, p. 3, SU 51/49+ cannot be considered “canonical, but are merely very close to the canonical series (cf. ibd, p. 6, fn. 57).')
+        self.assertEqual(self.sheet.comments.text[1],'gerû “to be hostile, to start a lawsuit”, “to bring suit and complain against” (CAD G: 61-62). The translation “who is litigating” is borrowed from CCP 3.5.22.A.b.')
+        return
+
+    def test_classification(self):
+        self.assertEqual(1,1)
         return
