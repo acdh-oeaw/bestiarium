38c9fb690e8ba274f92b6133cb1b33531c265dcd
gythaogg <gythaoggscat@gmail.com>
2019-08-13 16:23:57 +0200

    tested sheet to include comments correctly

--- tieromina/xl2tei/tests/test_sheet.py
+++ tieromina/xl2tei/tests/test_sheet.py
@@ -1,15 +1,23 @@
 from django.test import TestCase
 
 import xlrd
-from ..sheet import Sheet
+from ..sheet  import Sheet
+from ..comments import Comments
 
 class SheetTestCase(TestCase):
     def setUp(self):
         wb = xlrd.open_workbook('xl2tei/tests/Snake_23_1_11.xls',formatting_info=True)
-        self.sheet = Sheet(wb.sheet_by_index(0), line_num_format=2)
+        self.wb_sheet = wb.sheet_by_index(0)
+        self.sheet = Sheet(self.wb_sheet, line_num_format=2)
         return
 
     def test_line_num_format(self):
-        self.assertEqual(self.sheet.line_num_format, 2)
+        self.assertEqual(self.sheet .line_num_format, 2)
         return
 
+    def test_comment_lines(self):
+        expected_comment = Comments()
+        expected_comment.append(self.wb_sheet.row(21))
+        expected_comment.append(self.wb_sheet.row(22))
+        self.assertEqual(self.sheet .comments.__dict__, expected_comment.__dict__)
+        return
