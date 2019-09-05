95515bb11764e9f8dd3a0f5ea6b147553b8cb6e7
Saranya Balasubramanian <Saranya.Balasubramanian@oeaw.ac.at>
2019-09-05 14:00:41 +0200

    Created testcases for Sheet and Cell

---
+++ tieromina/xlsx2tei/tests/test_cell.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+from unittest.mock import PropertyMock, patch
+from xml.etree import ElementTree as ET
+
+from django.test import TestCase
+
+from ..cell import Cell
+
+
+class CellTestCase(TestCase):
+    pass
---
+++ tieromina/xlsx2tei/tests/test_sheet.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+from unittest.mock import PropertyMock, patch
+from xml.etree import ElementTree as ET
+
+from django.test import TestCase
+
+from ..sheet import Sheet
+
+
+class SheetTestCase(TestCase):
+    pass
