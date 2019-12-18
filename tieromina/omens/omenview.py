8922a3318644de1c24fb36b2ae81d1ea7bb05873
Saranya Balasubramanian <Saranya.Balasubramanian@oeaw.ac.at>
2019-12-18 14:54:42 +0100

    Moved hypernyms to omenview

--- tieromina/omens/omenview.py
+++ tieromina/omens/omenview.py
@@ -3,7 +3,16 @@ This acts as an interface between views.py and the model
 '''
 import logging
 
+import nltk
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import wordpunct_tokenize
+
 from .models import Chapter, Omen, Reconstruction, Translation
+
+wordnet_lemmatizer = WordNetLemmatizer()
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 
 
 def all_chapters() -> dict:
@@ -31,3 +40,37 @@ def get_chapter(chapter_name: str) -> Chapter:
     except IndexError:
         logging.error('Could not find chapter "%s"', chapter_name)
         return None
+
+
+def omen_hypernyms(omen_id: str) -> dict:
+    omen = Omen.objects.filter(omen_id=omen_id)[0]
+    translations = {}
+    senses = {}
+    for reading in Reconstruction.objects.filter(omen__omen_id=omen.omen_id):
+        translations[reading.reconstruction_id] = {}
+        senses[reading.reconstruction_id] = {}
+
+        records = Translation.objects.filter(
+            reconstruction__reconstruction_id=reading.reconstruction_id)
+        for record in records:
+            if record.segment.segment_id.endswith('P'):
+                segment_type = 'PROTASIS'
+            else:
+                segment_type = 'APODOSIS'
+
+            translations[reading.reconstruction_id][segment_type] = []
+            postags = nltk.pos_tag(wordpunct_tokenize(record.translation_txt))
+            print(postags)
+            for text, postag in postags:
+                sense_info = {'word': text, 'sense': []}
+                for sim in wordnet.synsets(text):
+                    print(text, sim.name(), sim.lemma_names())
+                    sense_info['sense'].append({
+                        'name': sim.hypernyms(),
+                        'lemmas': sim.lemma_names(),
+                        'examples': sim.examples()
+                    })
+
+                translations[reading.reconstruction_id][segment_type].append(
+                    sense_info)
+    return {'data': {'omen': omen, 'translations': translations}}
--- tieromina/omens/views.py
+++ tieromina/omens/views.py
@@ -1,21 +1,10 @@
-from collections import defaultdict
 
-import nltk
-import numpy as np
-import pandas as pd
-import spacy
 from django.shortcuts import render
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize, wordpunct_tokenize
 
 # Create your views here.
-from .models import Chapter, Omen, Reconstruction, Translation
-from .omenview import all_chapters, get_chapter, omens_in_chapter
-
-wordnet_lemmatizer = WordNetLemmatizer()
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
+from .models import Omen
+from .omenview import (all_chapters, get_chapter, omen_hypernyms,
+                       omens_in_chapter)
 
 
 def chapters(request):
@@ -32,47 +21,14 @@ def chapter_detail(request, chapter_name):
 
 def chapter_tei(request, chapter_name):
     template_name = 'omens/tei.xml'
-    chapter = get_chapter(chapter_name=chapter_name)
+    chaptenr = get_chapter(chapter_name=chapter_name)
     context = {'tei': chapter.tei}
     return render(request, template_name, context, content_type='text/xml')
 
 
 def omen_detail(request, omen_id):
     template_name = 'omens/omen_detail.html'
-    omen = Omen.objects.filter(omen_id=omen_id)[0]
-    translations = {}
-    senses = {}
-    for reading in Reconstruction.objects.filter(omen__omen_id=omen.omen_id):
-        print(reading)
-        translations[reading.reconstruction_id] = {}
-        senses[reading.reconstruction_id] = {}
-
-        records = Translation.objects.filter(
-            reconstruction__reconstruction_id=reading.reconstruction_id)
-        for record in records:
-            if record.segment.segment_id.endswith('P'):
-                segment_type = 'PROTASIS'
-            else:
-                segment_type = 'APODOSIS'
-
-            translations[reading.reconstruction_id][segment_type] = []
-            postags = nltk.pos_tag(wordpunct_tokenize(record.translation_txt))
-            print(postags)
-            for text, postag in postags:
-                sense_info = {'word': text, 'sense': []}
-                for sim in wordnet.synsets(text):
-                    print(text, sim.name(), sim.lemma_names())
-                    sense_info['sense'].append({
-                        'name': sim.hypernyms(),
-                        'lemmas': sim.lemma_names(),
-                        'examples': sim.examples()
-                    })
-
-                translations[reading.reconstruction_id][segment_type].append(
-                    sense_info)
-
-    print(translations)
-    context = {'data': {'omen': omen, 'translations': translations}}
+    context = omen_hypernyms(omen_id)
     return render(request, template_name, context, content_type='text/html')
 
 
