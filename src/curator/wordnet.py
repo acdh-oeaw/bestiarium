046ffa93f6bf8f479857507b7b63e7dab53552c9
Saranya Balasubramanian <Saranya.Balasubramanian@oeaw.ac.at>
2020-06-29 11:19:23 +0200

    Returning tree data instead of network data - fixed template

--- src/curator/wordnet.py
+++ src/curator/wordnet.py
@@ -9,54 +9,34 @@ from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import wordpunct_tokenize
 
+hyper = lambda s: s.hypernyms()
+hypo = lambda s: s.hyponyms()
 
-def get_hypernyms(segment_text) -> dict:
-    readings = {}
-    hypernyms = []
-    # collect wordnet senses
-    postags = pos_tag(wordpunct_tokenize(segment_text))
-    node_parent = {segment_text.upper(): []}
-    for text, postag in postags:
-        if len(text) < 3: continue
-        node_parent = parse_synsets(text, node_parent)
 
+def synset_tree(word):
+    node_id = 0
     G = DiGraph()
-    print(node_parent)
-    for k, v in node_parent.items():
-        G.add_node(k)
-        for vi in v:
-            G.add_node(vi)
-            G.add_edge(k, vi)
-
-    json_data = json_graph.node_link_data(G)
-
-    return dumps(json_data, ).replace('"', "\"")
+    G.add_node(node_id, value=word)
+    for s in wordnet.synsets(word):
+        tree = s.tree(hyper)
+        print(tree)
+        G, node_id = parse_synset_tree(tree, G, node_id, 0)
+        break
+    return json_graph.tree_data(G, root=0)
 
 
-def parse_synsets(word, node_parent):
-    hyp = lambda s: s.hypernyms()
-
-    for synset in wordnet.synsets(word):
-        node_parent[word.upper()].append(
-            synset.name())  # to connect the senses to the root word
-        tree = synset.tree(hyp)
-        node_parent = parse_tree(tree, synset.name(), node_parent)
-
-    return node_parent
-
-
-def parse_tree(tree, child, node_tree={}):
-    for element in tree:
-        if not isinstance(element, list):
-            if element.name() == child:
-                continue
-            else:
-                if child in node_tree:
-                    if element.name() not in node_tree[child]:
-                        node_tree[child].append(element.name())
-                else:
-                    node_tree[child] = [element.name()]
-                child = element.name()
+def parse_synset_tree(nested_tree, G, node_id, parent_node):
+    for item in nested_tree:
+        if isinstance(item, list):
+            G, node_id = parse_synset_tree(item, G, node_id, parent_node)
         else:
-            parse_tree(element, child, node_tree)
-    return node_tree
+            G.add_node(node_id + 1,
+                       value=item.name(),
+                       pos=item.pos(),
+                       defn=item.definition(),
+                       examples=item.examples())
+            G.add_edge(parent_node, node_id + 1)
+            node_id += 1
+            parent_node = node_id
+
+    return G, node_id
