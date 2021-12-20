f5771f9f7fe609e4a7a40835fe1d3433c3493d0e
csae8092 <peter.andorfer@oeaw.ac.at>
2021-12-20 16:51:54 +0100

    drop empty rows from comment df

--- omens/coment_utils.py
+++ omens/coment_utils.py
@@ -5,7 +5,8 @@ from omens.models import Omen, PhilComment
 
 def import_comments(df):
     ids = []
-    for i, row in df.iterrows():
+    for i, row in df.dropna().iterrows():
+        print(row)
         try:
             omen = Omen.objects.get(omen_num=row['omen'])
         except ObjectDoesNotExist:
