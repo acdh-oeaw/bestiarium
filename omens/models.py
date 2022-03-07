cff66a1ab633c57383d6e971b9fa4ca72388d1a1
Reinhard Pirngruber <reinhard.pirngruber@gmail.com>
2022-03-07 20:49:15 +0100

    previous commit undone

--- omens/models.py
+++ omens/models.py
@@ -291,8 +291,7 @@ class PhilComment(models.Model):
             note_node.attrib["{http://www.w3.org/XML/1998/namespace}id"] = self.xml_id
             note_node.text = self.comment
             return note_node
-        else:
-            return None
+        return None
 
     def get_parent_node(self):
         if self.omen.tei_content:
